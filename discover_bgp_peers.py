65a7cd22b363e6a2594c6ef770cc976ff07a50ff
Jan-Philipp Benecke <jan-philipp.benecke@jpbe.de>
2017-04-12 22:49:14 +0200

    added bird bgp peer discovery script

---
+++ discover_bgp_peers.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from pybird import PyBird
+from pprint import pprint
+
+pybird = PyBird(socket_file='/var/run/bird/bird.ctl')
+
+peers = pybird.get_peer_status()
+lineiterator = iter(peers)
+
+first = True
+
+print("{")
+print("\t\"data\":[")
+
+for peer in lineiterator:
+
+    if "description" in peer:
+       if not first:
+	  print(",")
+
+       print "\t\t{\"{#PEERNAME}\":\"%s\", \"{#PROTONAME}\":\"%s\" }" % (peer["description"], peer["name"])
+       first = False
+
+print("\t]")
+print("}")
